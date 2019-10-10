16eb2482feb7fe3c7dc0a2407f2281c6d7318620
kahstro <viniciusm0r3or4@gmail.com>
2019-10-10 10:21:46 -0300

    erro, não encontra app nem db

--- site.py
+++ site.py
@@ -1,5 +1,39 @@
 from flask import Flask, render_template
-app= Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.column(db.String(8), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" %self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/')
 def index():
